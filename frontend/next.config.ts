c29f9414192ec212ac4b11d15ee72cdddba04019
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2025-04-12 23:32:01 +0500

    fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  serverActions: {
-    bodySizeLimit: "10mb", 
-  },
   images: {
     remotePatterns: [
       {
