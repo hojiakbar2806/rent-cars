018138fa25a542d973528e1cf1d8f8da4cbddc0e
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2025-04-16 01:30:29 +0500

    fix

--- frontend/src/app/actions/cars/postLike.ts
+++ frontend/src/app/actions/cars/postLike.ts
@@ -4,15 +4,12 @@ import axios from "@/lib/axios";
 
 export default async function postLike(id: number, token: string | null) {
     try {
-        if (!token) throw new Error();
+        if (!token) throw new Error("Avval ro'yxatdan o'ting");
         const res = await axios.post(`/v1/favorites/${id}`, {}, {
             headers: { Authorization: `Bearer ${token}` },
         });
         return { message: res.data.message };
     } catch (error: unknown) {
-        if (axios.isAxiosError(error)) {
-            throw new Error(error.response?.data.detail[0].msg);
-        }
         throw new Error("Avval ro'yxatdan o'ting");
     }
 }
